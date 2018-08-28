c0b7d66189bdf617662aaa541c062078d4634db1
He4eT <He4eTHb1u@gmail.com>
2018-08-29 01:32:41 +0500

    some dotfiles and tools

---
+++ apply.sh
@@ -0,0 +1,23 @@
+# .Xresources
+ln -sv ~/dotfiles/root/.Xresources ~/.Xresources
+
+# .Xresources
+rm ~/.zshrc
+ln -sv ~/dotfiles/root/.zshrc ~/.zshrc
+
+# i3
+mkdir -p ~/.config/i3
+ln -sv ~/dotfiles/i3/config ~/.config/i3/config
+
+# polybar
+mkdir -p ~/.config/polybar
+ln -sv ~/dotfiles/polybar/config ~/.config/polybar/config
+
+# rofi
+mkdir -p ~/.config/rofi
+ln -sv ~/dotfiles/rofi/config ~/.config/rofi/config
+ln -sv ~/dotfiles/rofi/paper-float.rasi ~/.config/rofi/paper-float.rasi
+
+# oomox
+mkdir -p ~/.config/oomox/colors
+ln -sv ~/dotfiles/oomox/colors/gray ~/.config/oomox/colors/gray
---
+++ installPackages.sh
@@ -0,0 +1,14 @@
+sudo apt install \
+  rxvt-unicode-256color \
+  fonts-font-awesome \
+  xfonts-terminus \
+  lxappearance \
+  curl \
+  htop \
+  gimp \
+  node \
+  rofi \
+  git \
+  mc
+
+sudo update-alternatives --config x-terminal-emulator
