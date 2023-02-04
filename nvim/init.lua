47f4dbb291698b7b8ae7efd6dfe52991219d3f91
He4eT <He4eTHb1u@gmail.com>
2023-02-05 02:23:00 +0300

    nvim: leap labels

--- nvim/init.lua
+++ nvim/init.lua
@@ -399,8 +399,8 @@ require('leap').opts.highlight_unlabeled_phase_one_targets = true
 require('leap').opts.safe_labels = {}
 
 vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'Comment' })
-vim.api.nvim_set_hl(0, 'LeapLabelPrimary', { link = 'Cursor' })
-vim.api.nvim_set_hl(0, 'LeapMatch', { link = 'Identifier' })
+vim.api.nvim_set_hl(0, 'LeapLabelPrimary', { link = 'Identifier' })
+vim.api.nvim_set_hl(0, 'LeapMatch', { link = 'Constant' })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
