a42858ebe86e2bf7d12307c3d71166faf8b0b207
He4eT <He4eTHb1u@gmail.com>
2023-03-08 13:36:33 +0300

    nvim: replace sumneko_lua with lua_ls

--- nvim/init.lua
+++ nvim/init.lua
@@ -262,7 +262,7 @@ end
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- Enable the following language servers
-local servers = { 'tsserver', 'sumneko_lua' }
+local servers = { 'tsserver', 'lua_ls' }
 
 -- Ensure the servers above are installed
 require("mason").setup {}
@@ -288,7 +288,7 @@ local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, 'lua/?.lua')
 table.insert(runtime_path, 'lua/?/init.lua')
 
-require('lspconfig').sumneko_lua.setup {
+require('lspconfig').lua_ls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
   settings = {
