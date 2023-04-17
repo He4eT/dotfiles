cf511b3692a084dd8c11d3578757d8c1ff0084c4
He4eT <He4eT@oddsquat.org>
2023-04-17 12:16:09 +0300

    nvim: replace lsp markers with squares

--- nvim/init.lua
+++ nvim/init.lua
@@ -281,6 +281,12 @@ vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
   border = "solid",
 })
 
+local signs = { Error = "■ ", Warn = "■ ", Hint = "■ ", Info = "■ " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
 -- Example custom configuration for lua
 --
 -- Make runtime files discoverable to the server
