8b0c9631bd549b9a8cce215c25c89b9b7e871dba
He4eT <He4eT@oddsquat.org>
2026-06-03 12:51:52 +0200

    nvim: stylua

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -184,8 +184,8 @@ vim.keymap.set('n', '<leader>l', '<C-i>', { desc = 'Forward' })
 -- Copy'n'Paste
 vim.keymap.set('v', '<leader>y', '"+y', { desc = 'Cop[y] selection to system clipboard' })
 vim.keymap.set('n', '<leader>y', function()
-  vim.fn.setreg('+', vim.fn.getreg('"'))
-  print('Copied to system clipboard')
+  vim.fn.setreg('+', vim.fn.getreg '"')
+  print 'Copied to system clipboard'
 end, {
   silent = true,
   desc = 'Copy last [y]anked or deleted text to system clipboard',
